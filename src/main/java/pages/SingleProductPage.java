279c996624170fe61fb5488e6f5f39cb8aabc403
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-07-18 16:01:47 +0200

    Test Case 13

--- src/main/java/pages/SingleProductPage.java
+++ src/main/java/pages/SingleProductPage.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SingleProductPage extends AllProductsPage {
     private WebElement productDetails;
+    private WebElement quantityField;
+    private WebElement addToCartButton;
+    private WebElement viewCartButton;
 
     public SingleProductPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
@@ -15,5 +18,37 @@ public class SingleProductPage extends AllProductsPage {
     public WebElement getProductDetails() {
         productDetails = getDriver().findElement(By.className("product-information"));
         return productDetails;
+    }
+
+    public WebElement getQuantityField() {
+        quantityField = getDriver().findElement(By.id("quantity"));
+        return quantityField;
+    }
+
+    public WebElement getAddToCartButton() {
+        addToCartButton = getDriver().findElement(By.cssSelector(".btn.btn-default.cart"));
+        return addToCartButton;
+    }
+
+    @Override
+    public WebElement getViewCartButton() {
+        viewCartButton = getDriver().findElement(By.linkText("View Cart"));
+        return viewCartButton;
+    }
+
+    public SingleProductPage setQuantity(int quantity) {
+        getQuantityField().clear();
+        getQuantityField().sendKeys(Integer.toString(quantity));
+        return this;
+    }
+
+    public SingleProductPage addToCart() {
+        getAddToCartButton().click();
+        return this;
+    }
+
+    public SingleProductPage goToCart() {
+        getViewCartButton().click();
+        return this;
     }
 }
--- src/main/java/tests/CartTest.java
+++ src/main/java/tests/CartTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -31,6 +32,28 @@ public class CartTest extends BaseTest {
 
         getSoftAssert().assertEquals(totalPrice, getCartPage().getCartTotal(), "Total price is wrong");
         getSoftAssert().assertEquals(2, getCartPage().productsQuantity(), "Quantity does not match");
+        getSoftAssert().assertAll();
+    }
+
+    @Test(testName = "Test Case 13: Verify Product quantity in Cart")
+    public void verifyProductQuantity() {
+        getSoftAssert().assertTrue(getDriver().getTitle().contains("Automation Exercise"),
+                "Home page not visible successfully.");
+
+        int quantity = 4;
+        getNavigationPage().openAllProducts();
+        getAllProductsPage().viewProductByIndex(0);
+
+        getSoftAssert().assertTrue(getDriver().getCurrentUrl().contains("product_details"),
+                "Product details page not opened");
+
+        getSingleProductPage()
+                .setQuantity(quantity)
+                .addToCart()
+                .viewCart();
+
+        getSoftAssert().assertEquals(quantity, getCartPage().productsQuantity(),
+                "Product quantity does not match.");
         getSoftAssert().assertAll();
     }
 }
