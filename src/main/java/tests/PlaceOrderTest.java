a6e4515abe850a097fb01e7f1997843eedd2e16e
Milos Stamenkovic <milos_stamenkovic@outlook.com>
2023-07-21 18:48:07 +0200

    Test Case 16

--- src/main/java/tests/PlaceOrderTest.java
+++ src/main/java/tests/PlaceOrderTest.java
@@ -11,7 +11,7 @@ public class PlaceOrderTest extends BaseTest {
         getIndexPage().open();
     }
 
-    @Test(testName = "Test Case 14")
+    @Test(testName = "Test Case 14: Place Order: Register while Checkout")
     public void verifyPlaceOrderRegisterWhileCheckout() {
         getNavigationPage().openAllProducts();
         getAllProductsPage()
@@ -78,7 +78,7 @@ public class PlaceOrderTest extends BaseTest {
         getNavigationPage().deleteUser();
     }
 
-    @Test(testName = "Test Case 15")
+    @Test(testName = "Test Case 15: Place Order: Register before Checkout")
     public void verifyPlaceOrderRegisterBeforeCheckout() {
         getNavigationPage().openSignupLoginPage();
         String username = getFaker().name().username();
@@ -114,6 +114,80 @@ public class PlaceOrderTest extends BaseTest {
                 "ACCOUNT CREATED not visible.");
         getCreatedAccountPage().getContinueButton().click();
 
+        getSoftAssert().assertTrue(getNavigationPage().loggedInUsername().contains(username),
+                "Logged in as (username) not visible.");
+
+        getNavigationPage().openAllProducts();
+        getAllProductsPage()
+                .addProductToCart(1)
+                .viewCart();
+
+        getCartPage().proceedToCheckout();
+
+        getCheckoutPage()
+                .enterComment(getFaker().lorem().sentence())
+                .placeOrder();
+
+        getPaymentPage()
+                .enterNameOnCart(getFaker().name().fullName())
+                .enterCardNumber(getFaker().finance().creditCard())
+                .enterCVC("123")
+                .enterExpirationMonth("12")
+                .enterExpirationYear("2030")
+                .payAndConfirmOrder();
+
+
+        getSoftAssert().assertTrue(getDriver().getCurrentUrl().contains("payment_done"));
+        getSoftAssert().assertAll();
+
+        getNavigationPage().deleteUser();
+    }
+
+    @Test(testName = "Test Case 16: Place Order: Login before Checkout")
+    public void verifyPlaceOrderLoginBeforeCheckout() {
+        getIndexPage().goToSignupLogin();
+
+        String username = getFaker().name().username();
+        String password = getFaker().internet().password();
+        String email = getFaker().internet().emailAddress();
+
+        getSignupLoginPage().enterSignupName(username);
+        getSignupLoginPage().enterSignupEmail(email);
+        getSignupLoginPage().signup();
+
+        getSignupPage().getTitleMr().click();
+        getSignupPage().getPasswordField().sendKeys(password);
+        getSignupPage().getDayBirth().selectByValue("13");
+        getSignupPage().getMonthBirth().selectByValue("1");
+        getSignupPage().getYearBirth().selectByValue("1995");
+        getSignupPage().getSignUpForNewsletterCheck().click();
+        getSignupPage().getReceiveOffersCheck().click();
+        getSignupPage().getFirstNameField().sendKeys(getFaker().name().firstName());
+        getSignupPage().getLastNameField().sendKeys(getFaker().name().lastName());
+        getSignupPage().getCompanyField().sendKeys(getFaker().company().name());
+        getSignupPage().getAddressField().sendKeys(getFaker().address().fullAddress());
+        getSignupPage().getAddressTwoField().sendKeys(getFaker().address().fullAddress());
+        getSignupPage().getCountrySelect().selectByIndex(1);
+        getSignupPage().getStateField().sendKeys(getFaker().address().state());
+        getSignupPage().getCityField().sendKeys(getFaker().address().city());
+        getSignupPage().getZipcodeField().sendKeys(getFaker().address().zipCode());
+        getSignupPage().getMobileNumberField().sendKeys(getFaker().phoneNumber().phoneNumber());
+        getSignupPage().getCreateAccountButton().click();
+
+        getCreatedAccountPage().getContinueButton().click();
+        getNavigationPage().getLogoutButton().click();
+
+        getNavigationPage().getHomeButton().click();
+        getSoftAssert().assertTrue(getDriver().getTitle().contains("Automation Exercise"),
+                "Home page not visible successfully.");
+
+        getNavigationPage().getSignupLoginButton().click();
+        getSoftAssert().assertTrue(getSignupLoginPage().getLoginToYouAccountText().isDisplayed(),
+                "Login to your account not visible.");
+
+        getSignupLoginPage().enterLoginEmail(email);
+        getSignupLoginPage().enterLoginPassword(password);
+        getSignupLoginPage().login();
         getSoftAssert().assertTrue(getNavigationPage().loggedInUsername().contains(username),
                 "Logged in as (username) not visible.");
 
